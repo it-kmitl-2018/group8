fa30cf72ef0dd5bfc3a5d106c68a8f431cdf0794
pamai <pamai_5656@hotmail.com>
2018-02-12 20:46:01 +0700

    initialize kotlin with spring boot framework

---
+++ src/main/kotlin/com/example/group8/Group8Application.kt
@@ -0,0 +1,11 @@
+package com.example.group8
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Group8Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Group8Application::class.java, *args)
+}
