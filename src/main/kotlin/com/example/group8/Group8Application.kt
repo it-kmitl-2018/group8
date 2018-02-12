7e1d17afa98bda710a2e0c866c9dfc3c8ef36d60
Ben5497 <35713168+Ben5497@users.noreply.github.com>
2018-02-12 23:47:41 +0700

    add example service

--- src/main/kotlin/com/example/group8/Group8Application.kt
+++ src/main/kotlin/com/example/group8/Group8Application.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Group8Application
+open class Group8Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Group8Application::class.java, *args)
---
+++ src/main/kotlin/com/example/group8/Group8Service.kt
@@ -0,0 +1,8 @@
+package com.example.group8
+
+import org.springframework.stereotype.Service
+
+@Service
+class Group8Service {
+    fun getHello() = "Hello Service"
+}
