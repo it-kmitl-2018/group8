8efdc0958315ff1f2448d42c8c13b7a38180c0d8
Thasorn Chalongvorachai <s8070047@kmitl.ac.th>
2018-03-14 09:15:27 +0700

    Edit variables due to change of requirement
    Edit variables due to change of requirement
    Note : We also delete GuidelinespecifiedID since it will never been used angain.

--- src/components/invoiceForm.vue
+++ src/components/invoiceForm.vue
@@ -2,70 +2,44 @@
    <div class="invoiceForm">
      <div v-if="buttonIsSubmitted===false">
        <br>
-       <h1>ใบแจ้งหนี้และใบรับอิเล็กทรอนิกส์</h1>
-       <guideline-specified :guidelineSpecifiedID="guidelineSpecifiedID"></guideline-specified>
-       <!--CIIH Exchanged Document-->
-       <h3>หัวเรื่องเอกสาร</h3>
-       เลขที่เอกสาร : <input v-model="exchangedDocument.id"/><br>
-       ชื่อเอกสาร : <input v-model="exchangedDocument.name"/><br>
-       รหัสประเภทเอกสาร : <input v-model="exchangedDocument.typeCode"/><br>
-       วันเดือนปี ที่ออกเอกสาร  : <input v-model="exchangedDocument.issueDateTime"/><br>
-       สาเหตุการออกเอกสาร : <input v-model="exchangedDocument.purpose"/><br>
-       รหัสสาเหตุการออกเอกสาร : <input v-model="exchangedDocument.purposeCode"/><br>
-       เลขที่เอกสารสากล : <input v-model="exchangedDocument.globalID"/><br>
-       วันเดือนปีและเวลาที่สร้างเอกสาร : <input v-model="exchangedDocument.creationDateTime"/><br>
-       <!--Included Note-->
-       <h3>ข้อความเพิ่มเติม</h3>
-       หัวข้อ : <input v-model="includeNote.subject"/><br>
-       เนื้อหา : <textarea v-model="includeNote.content"/><br>
+       <h1>ใบกำกับภาษีอิเล็กทรอนิกส์</h1>
+       <!--Seller-->
+       <h2>ผู้ซื้อ</h2>
+       <!--Seller Trade Party-->
+       รหัสผู้ค้า(ผู้ขาย) : <input v-model="seller.id"/><br>
+       รหัสผู้ค้าสากล(ผู้ขาย) : <input v-model="seller.globalID"/><br>
+       ชื่อผู้ขาย : <input v-model="seller.name"/><br>
+       <!--Specified Tax Registration-->
+       เลขประจำตัวผู้เสียภาษีอากร : <input v-model="seller.taxID"/><br>
        <button @click="buttonIsSubmitted=true"> ส่งข้อมูล </button>
      </div>
      <div v-else>
         <h1> โปรดตรวจสอบข้อมูลดังกล่าวว่าถูกต้องหรือไม่ </h1>
         <!--Guideline Specified CI Document Context Parameter -->
-        <h3>ข้อมูลคู่มือมาตรฐาน</h3>
-        รหัสคู่มือมาตรฐาน : <span>{{ guidelineSpecifiedID }}</span><br>
-        <!--CIIH Exchanged Document-->
-        <h3>หัวเรื่องเอกสาร</h3>
-        เลขที่เอกสาร : <span>{{ exchangedDocument.id }}</span><br>
-        ชื่อเอกสาร : <span>{{ exchangedDocument.name }}</span><br>
-        รหัสประเภทเอกสาร : <span>{{ exchangedDocument.typeCode }}</span><br>
-        วันเดือนปี ที่ออกเอกสาร  : <span>{{ exchangedDocument.issueDateTime }}</span><br>
-        สาเหตุการออกเอกสาร : <span>{{ exchangedDocument.purpose }}</span><br>
-        รหัสสาเหตุการออกเอกสาร : <span>{{ exchangedDocument.purposeCode }}</span><br>
-        เลขที่เอกสารสากล : <span>{{ exchangedDocument.globalID }}</span><br>
-        วันเดือนปีและเวลาที่สร้างเอกสาร : <span>{{ exchangedDocument.creationDateTime }}</span><br>
-        <!--Included Note-->
-        <h3>ข้อความเพิ่มเติม</h3>
-        หัวข้อ : <span>{{ includeNote.subject }}</span><br>
-        เนื้อหา : <span>{{ includeNote.content }}</span><br>
+        <h1>ใบกำกับภาษีอิเล็กทรอนิกส์</h1>
+        <h2>ผู้ซื้อ</h2>
+        รหัสคู่มือมาตรฐาน : <span>{{ seller.id }}</span><br>
+        <!--Seller Trade Party-->
+        รหัสผู้ค้า(ผู้ขาย) : <span>{{ seller.id }}</span><br>
+        รหัสผู้ค้าสากล(ผู้ขาย) : <span>{{ seller.globalID }}</span><br>
+        ชื่อผู้ขาย : <span>{{ seller.name }}</span><br>
+        <!--Specified Tax Registration-->
+        เลขประจำตัวผู้เสียภาษีอากร : <span>{{ seller.taxID }}</span><br>
+
      </div>
    </div>
 </template>
 <script>
-  import GuidelineSpecified from '@/components/GuidelineSpecifiedID.vue'
 
   export default {
-    components: {
-      GuidelineSpecified
-    },
     data () {
       return {
         buttonIsSubmitted: false,
-        guidelineSpecifiedID: '',
-        exchangedDocument: {
+        seller: {
           id: '',
-          name: '',
-          typeCode: '',
-          issueDateTime: '',
-          purpose: '',
-          purposeCode: '',
           globalID: '',
-          creationDateTime: ''
-        },
-        includeNote: {
-          subject: '',
-          content: ''
+          name: '',
+          taxID: ''
         }
       };
     },
