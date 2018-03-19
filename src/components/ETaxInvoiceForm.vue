78f4d0636916bc3345b02526f4a03d2a076a9b9b
Thasorn Chalongvorachai <s8070047@kmitl.ac.th>
2018-03-19 20:55:17 +0700

    Seperate confirm components
    Seperate confirm components. Start with ConfirmSellerInfo.vue. I also catagorize file with form_pages folder and confirm_pages folder

--- src/components/ETaxInvoiceForm.vue
+++ src/components/ETaxInvoiceForm.vue
@@ -3,43 +3,40 @@
      <div v-if="buttonIsSubmitted===false">
        <br>
        <h1>ใบกำกับภาษีอิเล็กทรอนิกส์</h1>
-       <seller-info-form :sellerInfoForm="seller"></seller-info-form>
+       <seller-info-form :sellerInfoForm="form.seller"></seller-info-form>
        <button @click="buttonIsSubmitted=true"> ส่งข้อมูล </button>
      </div>
      <div v-else>
         <h1> โปรดตรวจสอบข้อมูลดังกล่าวว่าถูกต้องหรือไม่ </h1>
-        <!--Guideline Specified CI Document Context Parameter -->
-        <h1>ใบกำกับภาษีอิเล็กทรอนิกส์</h1>
-        <h2>ผู้ขาย</h2>
-        <!--Seller Trade Party-->
-        รหัสผู้ค้า(ผู้ขาย) : <span>{{ seller.id }}</span><br>
-        รหัสผู้ค้าสากล(ผู้ขาย) : <span>{{ seller.globalID }}</span><br>
-        ชื่อผู้ขาย : <span>{{ seller.name }}</span><br>
-        <!--Specified Tax Registration-->
-        เลขประจำตัวผู้เสียภาษีอากร : <span>{{ seller.taxID }}</span><br>
+        <h2>ใบกำกับภาษีอิเล็กทรอนิกส์</h2>
+        <confirm-seller-info :sellerInfoConfirm="form"></confirm-seller-info>
      </div>
    </div>
 </template>
 
 <script>
-  import SellerInfoForm from '@/components/SellerInfoForm.vue'
+  import SellerInfoForm from '@/components/form_pages/SellerInfoForm.vue'
+  import ConfirmSellerInfo from '@/components/confirm_pages/ConfirmSellerInfo.vue'
 
   export default {
     data () {
       return {
         buttonIsSubmitted: false,
-        seller: {
-          id: '',
-          globalID: '',
-          name: '',
-          taxID: ''
+        form: {
+          seller: {
+            id: '',
+            globalID: '',
+            name: '',
+            taxID: ''
+          }
         }
       };
     },
     components: {
-      SellerInfoForm
+      SellerInfoForm,
+      ConfirmSellerInfo
     },
     methods: {
-    }
+    },
   }
 </script>
