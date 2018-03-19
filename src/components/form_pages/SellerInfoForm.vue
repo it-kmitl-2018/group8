a65f16d126bb4c11977729114354f14e0fb25863
Thasorn Chalongvorachai <s8070047@kmitl.ac.th>
2018-03-19 21:11:05 +0700

    Seperate confirm components
    Seperate confirm components. Start with SellerInfoConfirm.vue. I also catagorized components  file by form_pages folder and confirm_pages folder.

---
+++ src/components/form_pages/SellerInfoForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!--Seller-->
+    <h2>ผู้ขาย</h2>
+    <!--Seller Trade Party-->
+    รหัสผู้ค้า(ผู้ขาย) : <input type="text" maxlength="35" v-model="sellerInfoForm.id"/><br>
+    รหัสผู้ค้าสากล(ผู้ขาย) : <input type="text" maxlength="70" v-model="sellerInfoForm.globalID"/><br>
+    ชื่อผู้ขาย : <input type="text" maxlength="256" v-model="sellerInfoForm.name"/><br>
+    <!--Specified Tax Registration-->
+    เลขประจำตัวผู้เสียภาษีอากร : <input type="text" maxlength="35" v-model="sellerInfoForm.taxID"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['sellerInfoForm']
+}
+</script>
