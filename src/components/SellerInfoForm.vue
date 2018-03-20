5525ac6bc429e1dc2b4fe367dc6a3478f09a06b3
Thasorn Chalongvorachai <s8070047@kmitl.ac.th>
2018-03-20 10:03:30 +0700

    Resolve conflict at SellerInfoForm
    Resolve conflict at SellerInfoForm by delete attribute.

--- src/components/SellerInfoForm.vue
+++ src/components/SellerInfoForm.vue
@@ -3,11 +3,11 @@
     <!--Seller-->
     <h2>ผู้ขาย</h2>
     <!--Seller Trade Party-->
-    รหัสผู้ค้า(ผู้ขาย) : <input type="text" maxlength="35" v-model="sellerInfoForm.id"/><br>
-    รหัสผู้ค้าสากล(ผู้ขาย) : <input type="text" maxlength="70" v-model="sellerInfoForm.globalID"/><br>
-    ชื่อผู้ขาย : <input type="text" maxlength="256" v-model="sellerInfoForm.name"/><br>
+    รหัสผู้ค้า(ผู้ขาย) : <input v-model="sellerInfoForm.id"/><br>
+    รหัสผู้ค้าสากล(ผู้ขาย) : <input v-model="sellerInfoForm.globalID"/><br>
+    ชื่อผู้ขาย : <input v-model="sellerInfoForm.name"/><br>
     <!--Specified Tax Registration-->
-    เลขประจำตัวผู้เสียภาษีอากร : <input type="text" maxlength="35" v-model="sellerInfoForm.taxID"/><br>
+    เลขประจำตัวผู้เสียภาษีอากร : <input v-model="sellerInfoForm.taxID"/><br>
   </div>
 </template>
 
