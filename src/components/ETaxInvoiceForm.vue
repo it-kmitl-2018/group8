07a281f4aa19b2f1c3c1edd2c9151ab29ef006d3
Thasorn Chalongvorachai <s8070047@kmitl.ac.th>
2018-03-20 09:53:39 +0700

    Edit seller data object name
    Edit seller object name. From seller to sellerInfo.
    The reason I decide to change the name because we are going to have some information about seller such as address in future. In my opinion, it would be better if we split them into 2 data object.
    This

--- src/components/ETaxInvoiceForm.vue
+++ src/components/ETaxInvoiceForm.vue
@@ -3,45 +3,40 @@
      <div v-if="buttonIsSubmitted===false">
        <br>
        <h1>ใบกำกับภาษีอิเล็กทรอนิกส์</h1>
-       <!--Seller-->
-       <h2>ผู้ซื้อ</h2>
-       <!--Seller Trade Party-->
-       รหัสผู้ค้า(ผู้ขาย) : <input v-model="seller.id"/><br>
-       รหัสผู้ค้าสากล(ผู้ขาย) : <input v-model="seller.globalID"/><br>
-       ชื่อผู้ขาย : <input v-model="seller.name"/><br>
-       <!--Specified Tax Registration-->
-       เลขประจำตัวผู้เสียภาษีอากร : <input v-model="seller.taxID"/><br>
+       <seller-info-form :sellerInfoForm="sellerInfo"></seller-info-form>
        <button @click="buttonIsSubmitted=true"> ส่งข้อมูล </button>
      </div>
      <div v-else>
         <h1> โปรดตรวจสอบข้อมูลดังกล่าวว่าถูกต้องหรือไม่ </h1>
         <!--Guideline Specified CI Document Context Parameter -->
         <h1>ใบกำกับภาษีอิเล็กทรอนิกส์</h1>
-        <h2>ผู้ซื้อ</h2>
-        รหัสคู่มือมาตรฐาน : <span>{{ seller.id }}</span><br>
+        <h2>ผู้ขาย</h2>
         <!--Seller Trade Party-->
-        รหัสผู้ค้า(ผู้ขาย) : <span>{{ seller.id }}</span><br>
-        รหัสผู้ค้าสากล(ผู้ขาย) : <span>{{ seller.globalID }}</span><br>
-        ชื่อผู้ขาย : <span>{{ seller.name }}</span><br>
+        รหัสผู้ค้า(ผู้ขาย) : <span>{{ sellerInfo.id }}</span><br>
+        รหัสผู้ค้าสากล(ผู้ขาย) : <span>{{ sellerInfo.globalID }}</span><br>
+        ชื่อผู้ขาย : <span>{{ sellerInfo.name }}</span><br>
         <!--Specified Tax Registration-->
-        เลขประจำตัวผู้เสียภาษีอากร : <span>{{ seller.taxID }}</span><br>
-
+        เลขประจำตัวผู้เสียภาษีอากร : <span>{{ sellerInfo.taxID }}</span><br>
      </div>
    </div>
 </template>
 <script>
+  import SellerInfoForm from '@/components/form_pages/SellerInfoForm.vue'
 
   export default {
     data () {
       return {
         buttonIsSubmitted: false,
-        seller: {
+        sellerInfo: {
           id: '',
           globalID: '',
           name: '',
           taxID: ''
         }
       };
+    },
+    components: {
+      SellerInfoForm
     },
     methods: {
     }
