d75f257288665386c39fff4b16874b8cacef2fe7
Thasorn Chalongvorachai <s8070047@kmitl.ac.th>
2018-03-28 20:43:31 +0700

    Change seller confirm section into components
    Change seller confirm section into components. This also include new folder. form_pages and confirm_pages.

--- src/components/ETaxInvoiceForm.vue
+++ src/components/ETaxInvoiceForm.vue
@@ -10,40 +10,14 @@
         <h1> โปรดตรวจสอบข้อมูลดังกล่าวว่าถูกต้องหรือไม่ </h1>
         <!--Guideline Specified CI Document Context Parameter -->
         <h1>ใบกำกับภาษีอิเล็กทรอนิกส์</h1>
-        <h2>ผู้ขาย</h2>
-        <!--Seller Trade Party-->
-        รหัสผู้ค้า(ผู้ขาย) : <span>{{ seller.id }}</span><br>
-        รหัสผู้ค้าสากล(ผู้ขาย) : <span>{{ seller.globalID }}</span><br>
-        ชื่อผู้ขาย : <span>{{ seller.name }}</span><br>
-        <!--Specified Tax Registration-->
-        เลขประจำตัวผู้เสียภาษีอากร : <span>{{ seller.taxID }}</span><br>
-        <!--Defined Trade Contact-->
-        ชื่อผู้ติดต่อ: <span>{{ seller.personName }}</span><br>
-        ชื่อแผนก: <span>{{ seller.departmentName }}</span><br>
-        <!--E-mail URI Universal Communication-->
-        อีเมล: <span>{{ seller.email }}</span><br>
-        <!--Telephone Universal Communication -->
-        เบอร์โทรศัพท์: <span>{{ seller.phoneNumber }}</span><br>
-        <!--Postal Trade Address -->
-        รหัสไปรษณีย์: <span>{{ seller.postalCode }}</span><br>
-        ชื่ออาคาร: <span>{{ seller.building }}</span><br>
-        ที่อยู่บรรทัดที่ 1: <span>{{ seller.addressLineOne }}</span><br>
-        ที่อยู่บรรทัดที่ 2: <span>{{ seller.addressLineTwo }}</span><br>
-        ซอย: <span>{{ seller.addressLineThree }}</span><br>
-        หมู่บ้าน: <span>{{ seller.addressLineFour }}</span><br>
-        หมู่ที่: <span>{{ seller.addressLineFive }}</span><br>
-        ถนน: <span>{{ seller.street }}</span><br>
-        ชื่ออำเภอ: <span>{{ seller.district }}</span><br>
-        ชื่อตำบล: <span>{{ seller.subDistrict }}</span><br>
-        รหัสประเทศ: <span>{{ seller.countryID }}</span><br>
-        รหัสจังหวัด: <span>{{ seller.provinceID }}</span><br>
-        บ้านเลขที่: <span>{{ seller.houseNumber }}</span><br>
+        <seller-info-confirm></seller-info-confirm>
      </div>
    </div>
 </template>
 <script>
-  import SellerInfoForm from '@/components/SellerInfoForm.vue'
+  import SellerInfoForm from '@/components/form_pages/SellerInfoForm.vue'
   import SellerData from '@/data/Seller.data.js'
+  import SellerInfoConfirm from '@/components/confirm_pages/SellerInfoConfirm.vue'
 
   export default {
     data () {
@@ -53,7 +27,8 @@
       };
     },
     components: {
-      SellerInfoForm
+      SellerInfoForm,
+      SellerInfoConfirm
     },
     methods: {
     }
