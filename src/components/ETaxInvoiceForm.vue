c00b4751385452c66cc8929d24e487b75dc05d79
Thasorn Chalongvorachai <s8070047@kmitl.ac.th>
2018-03-31 22:13:21 +0700

    Swap import line
    Swap import line in ETaxInvoiceForm.vue to make it easier to read.

--- src/components/ETaxInvoiceForm.vue
+++ src/components/ETaxInvoiceForm.vue
@@ -16,11 +16,11 @@
    </div>
 </template>
 <script>
-import SellerInfoForm from '@/components/form_pages/SellerInfoForm.vue'
-import RecipientInfoForm from '@/components/form_pages/RecipientInfoForm.vue'
-import SellerData from '@/data/Seller.data.js'
-import RecipientData from '@/data/Recipient.data.js'
 import SellerInfoConfirm from '@/components/confirm_pages/SellerInfoConfirm.vue'
+import SellerInfoForm from '@/components/form_pages/SellerInfoForm.vue'
+import SellerData from '@/data/Seller.data.js'
+import RecipientInfoForm from '@/components/form_pages/RecipientInfoForm.vue'
+import RecipientData from '@/data/Recipient.data.js'
 
 export default {
   data () {
