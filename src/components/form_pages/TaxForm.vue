8061b1fec682646362a2afc406b787a80668fbf5
Thasorn Chalongvorachai <s8070047@kmitl.ac.th>
2018-04-15 14:25:35 +0700

    Create tax form component
    Create tax form component

---
+++ src/components/form_pages/TaxForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>ข้อมูลภาษี</h2>
+    รหัสประเภทภาษี : <input v-model="taxFormProp.taxCode"/><br>
+    อัตราภาษี : <input v-model="taxFormProp.taxRate"/><br>
+    มูลค่าสินค้าบริการ (ไม่รวมภาษี) : <input v-model="taxFormProp.valueAmount"/><br>
+    มูลต่าภาษีมูลค่าเพิ่ม: <input v-model="taxFormProp.vatAmount"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['taxFormProp']
+}
+</script>
