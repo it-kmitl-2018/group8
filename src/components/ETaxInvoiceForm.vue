78d9b732dd45a3eb53b23b2e04e861cf60828a64
Thasorn Chalongvorachai <s8070047@kmitl.ac.th>
2018-04-15 21:15:26 +0700

    Change Tax to TaxInvoice
    Change Tax to TaxInvoice

--- src/components/ETaxInvoiceForm.vue
+++ src/components/ETaxInvoiceForm.vue
@@ -4,7 +4,7 @@
       <h1>ใบกำกับภาษีอิเล็กทรอนิกส์</h1>
       <seller-form :sellerFormProp="seller"></seller-form>
       <recipient-form :recipientFormProp="recipient"></recipient-form>
-      <tax-form :taxFormProp="tax"></tax-form>
+      <tax-invoice-form :taxFormProp="taxInvoice"></tax-invoice-form>
       <document-recipient-form :documentRecipientFormProp="documentRecipient"></document-recipient-form>
       <payee-form :payeeFormProp="payee"></payee-form>
       <router-link :to="{name: 'ETaxInvoiceConfirm'}">
@@ -17,8 +17,8 @@ import SellerForm from '@/components/form_pages/SellerForm.vue'
 import SellerData from '@/data/Seller.data.js'
 import RecipientForm from '@/components/form_pages/RecipientForm.vue'
 import RecipientData from '@/data/Recipient.data.js'
-import TaxForm from '@/components/form_pages/TaxForm.vue'
-import TaxData from '@/data/Tax.data.js'
+import TaxInvoiceForm from '@/components/form_pages/TaxInvoiceForm.vue'
+import TaxInvoiceData from '@/data/TaxInvoice.data.js'
 import DocumentRecipientForm from '@/components/form_pages/DocumentRecipientForm.vue'
 import DocumentRecipientData from '@/data/DocumentRecipient.data.js'
 import PayeeForm from '@/components/form_pages/PayeeForm.vue'
@@ -29,7 +29,7 @@ export default {
     return {
       seller: SellerData.data,
       recipient: RecipientData.data,
-      tax: TaxData.data,
+      taxInvoice: TaxInvoiceData.data,
       documentRecipient: DocumentRecipientData.data,
       payee: PayeeData.data
     }
@@ -37,7 +37,7 @@ export default {
   components: {
     SellerForm,
     RecipientForm,
-    TaxForm,
+    TaxInvoiceForm,
     DocumentRecipientForm,
     PayeeForm
   },
