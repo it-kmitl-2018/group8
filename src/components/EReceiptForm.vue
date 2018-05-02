0d97c0371498974120bc5fde0ebc314ee0d79928
bboomber <nutnichasexy@gmail.com>
2018-05-02 19:42:10 +0700

    Add EReceipt
    สร้าง EReceiptForm.vue
    แก้ไขApp.vue และ index.js ให้แสดงเมนูใบเสร็จ

---
+++ src/components/EReceiptForm.vue
@@ -0,0 +1,47 @@
+<template>
+   <div class="EReceiptForm">
+      <br>
+      <h1>ใบเสร็จอิเล็กทรอนิกส์</h1>
+      <seller-form :sellerFormProp="seller"></seller-form>
+      <recipient-form :recipientFormProp="recipient"></recipient-form>
+      <tax-invoice-form :taxInvoiceFormProp="taxInvoice"></tax-invoice-form>
+      <document-recipient-form :documentRecipientFormProp="documentRecipient"></document-recipient-form>
+      <payee-form :payeeFormProp="payee"></payee-form>
+      <router-link :to="{name: 'ETaxInvoiceConfirm'}">
+        <button> ส่งข้อมูล </button>
+      </router-link>
+   </div>
+</template>
+<script>
+import SellerForm from '@/components/form_pages/SellerForm.vue'
+import SellerData from '@/data/Seller.data.js'
+import RecipientForm from '@/components/form_pages/RecipientForm.vue'
+import RecipientData from '@/data/Recipient.data.js'
+import TaxInvoiceForm from '@/components/form_pages/TaxInvoiceForm.vue'
+import TaxInvoiceData from '@/data/TaxInvoice.data.js'
+import DocumentRecipientForm from '@/components/form_pages/DocumentRecipientForm.vue'
+import DocumentRecipientData from '@/data/DocumentRecipient.data.js'
+import PayeeForm from '@/components/form_pages/PayeeForm.vue'
+import PayeeData from '@/data/Payee.data.js'
+
+export default {
+  data () {
+    return {
+      seller: SellerData.data,
+      recipient: RecipientData.data,
+      taxInvoice: TaxInvoiceData.data,
+      documentRecipient: DocumentRecipientData.data,
+      payee: PayeeData.data
+    }
+  },
+  components: {
+    SellerForm,
+    RecipientForm,
+    TaxInvoiceForm,
+    DocumentRecipientForm,
+    PayeeForm
+  },
+  methods: {
+  }
+}
+</script>
