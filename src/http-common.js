d278a5bf36d6142fb3e0e42bcc412e450ac4421b
pamai <pamai_5656@hotmail.com>
2018-05-07 17:13:11 +0700

    Change baseURL(Backend) port

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const AXIOS = axios.create({
-  baseURL: `http://localhost:8000`,
+  baseURL: `http://localhost:8888`,
   headers: {
     'Access-Control-Allow-Origin': 'http://localhost:8080'
   }
