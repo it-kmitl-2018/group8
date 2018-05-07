30afbb725249deac95ad1d994053f9deef7c678c
pamai <pamai_5656@hotmail.com>
2018-05-07 22:22:10 +0700

    Remove header

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,4 @@ import axios from 'axios'
 
 export const AXIOS = axios.create({
   baseURL: `http://localhost:8888`,
-  headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:8080'
-  }
 })
